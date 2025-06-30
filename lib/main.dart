d258bafe62154c94dd6821f836c2b80320574dd6
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-06-30 21:28:28 +0530

    added stateless widget Home

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,36 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My Flutter App'),
+          title: const Text(
+            'My Flutter App',
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           backgroundColor: Colors.brown[800],
           centerTitle: true,
         ),
-        body: const Center(child: Text('Hello, Flutter!')),
+        body: const Home(),
       ),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Hello, Flutter!',
+      style: TextStyle(
+        fontSize: 20,
+        color: Colors.brown,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
 }
